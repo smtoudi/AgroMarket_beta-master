d5558cd8450c0c27fd9c5cc7b74faa75252c9f24
slawomir.makurat <Laseczka1>
2017-03-14 23:10:13 +0100

    format code

--- app/src/main/java/com/example/slawomirmakurat/agromarket/MainActivity.java
+++ app/src/main/java/com/example/slawomirmakurat/agromarket/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.slawomirmakurat.agromarket;
 
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
@@ -16,8 +18,10 @@ import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
@@ -35,36 +39,30 @@ import com.example.slawomirmakurat.agromarket.drawer.DrawerSettings;
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
 
-
     private ViewPager viewPager;
     private DrawerLayout drawer;
     private Toolbar toolbar;
-
+    private TabLayout tabLayout;
+    private FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Poniższe 2 linijki ustawiają Toolbar jako systemy ActionBar
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
-        viewPager = (ViewPager) findViewById(R.id.content_main);
-        viewPager.setAdapter(new MaterialPagerAdapter(this, getSupportFragmentManager()));
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
-        tabLayout.setupWithViewPager(viewPager);
-
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle(null);
-//
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        // znikający FAB
-//        CoordinatorLayout.LayoutParams p = (CoordinatorLayout.LayoutParams) fab.getLayoutParams();
-//        p.setBehavior(new ScrollAwareFABBehavior(ontext, attrs, int defStyleAttr));
-//        fab.setLayoutParams(p);
 
+        viewPager = (ViewPager) findViewById(R.id.content_main);
+        viewPager.setAdapter(new MaterialPagerAdapter(this, getSupportFragmentManager()));
+
+        tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        tabLayout.setupWithViewPager(viewPager);
+
+
+        fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -76,36 +74,25 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 }).show();
             }
         });
+
         drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
 
-        // Poniższe 3 linijku kodu ustawiają "burger icon" w naszym ActionBar
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.nav_open, R.string.nav_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-
-        //ustawiamy obiekt implementujący interface NavigationView.OnNavigationItemSelectedListener
         navigationView.setNavigationItemSelectedListener(this);
 
-        // Pobieramy zasób typu BitmapDrawable i pobieramy z niego bitmapę
         Bitmap bitmap = ((BitmapDrawable) getDrawable(R.drawable.avatar_default)).getBitmap();
 
-        // Tworzymy obiekt typu RoundedBitmapDrawable przy użyciu naszej bitmapy i ustawiamy jego
-        // kształt (za pomocą kanału alpha czyli przezroczystości) na okgrągły
         RoundedBitmapDrawable avatar = RoundedBitmapDrawableFactory.create(getResources(), bitmap);
         avatar.setCircular(true);
 
-        // Wyświetlamy okrągły avatar użytkownika w ImageView pobiranym z layoutu nagłówka w naszym NavigationView
         ImageView avatarView = (ImageView) navigationView.getHeaderView(0).findViewById(R.id.avatar);
-    avatarView.setImageDrawable(avatar);
-}
+        avatarView.setImageDrawable(avatar);
+    }
 
-    /**
-     * Metoda onBackPressed() wykonywana jest po wciśnięciu prez użytkownika przycisku "wstecz".
-     * Sprawdzamy czy drawer jest otwarty. Jeśli jest otwarty, zamykamy go i kończymy działanie metody,
-     * w przeciwnym wypadku kierujemy akcję do metody w klasie po której dziedziczy ta aktywność.
-     */
     @Override
     public void onBackPressed() {
         if (drawer.isDrawerOpen(GravityCompat.START)) {
@@ -113,14 +100,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         } else {
             super.onBackPressed();
         }
-    }
-    /**
-     * Wyświetlamy menu w ActionBarze
-     */
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_toolbar, menu);
-        return true;
     }
 
     /**
@@ -134,68 +113,76 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 Snackbar.make(drawer, R.string.some_settings, Snackbar.LENGTH_SHORT).show();
                 break;
             case R.id.action_create_account:
-                Intent i = new  Intent(this, LoginActivity.class);
-                startActivity(i);;
+                Intent i = new Intent(this, LoginActivity.class);
+                startActivity(i);
 
         }
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * Przechwytujemy kliknięcia w naszej nawigacji.
-     * Jest to implementacja interfejsu NavigationView.OnNavigationItemSelectedListener
-     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.activity_toolbar, menu);
+
+//        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+//        SearchView searchView = (SearchView) menu.findItem(R.id.search).getActionView();
+//        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+
+        return true;
+    }
+
 
     @Override
-    public boolean onNavigationItemSelected( MenuItem item) {
+    public boolean onNavigationItemSelected(MenuItem item) {
         int id = item.getItemId();
 
         if (id == R.id.action_home) {
-            Intent i = new  Intent(this, MainActivity.class);
+            Intent i = new Intent(this, MainActivity.class);
             startActivity(i);
         }
 
         if (id == R.id.action_navig) {
-            Intent i = new  Intent(this, DrawerNavig.class);
+            Intent i = new Intent(this, DrawerNavig.class);
             startActivity(i);
         }
 
         if (id == R.id.nav_add_frend) {
-            Intent i = new  Intent(this, DrawerAddFrend.class);
+            Intent i = new Intent(this, DrawerAddFrend.class);
             startActivity(i);
         }
         if (id == R.id.action_message) {
-            Intent i = new  Intent(this, DrawerNavig.class);
+            Intent i = new Intent(this, DrawerNavig.class);
             startActivity(i);
         }
 
         if (id == R.id.action_my_ads) {
-            Intent i = new  Intent(this, DrawerMyAds.class);
+            Intent i = new Intent(this, DrawerMyAds.class);
             startActivity(i);
         }
 
         if (id == R.id.action_my_favprite) {
-            Intent i = new  Intent(this, DrawerMyFavorite.class);
+            Intent i = new Intent(this, DrawerMyFavorite.class);
             startActivity(i);
         }
 
         if (id == R.id.action_lav) {
-            Intent i = new  Intent(this, DrawerLav.class);
+            Intent i = new Intent(this, DrawerLav.class);
             startActivity(i);
         }
 
         if (id == R.id.action_add_ad) {
-            Intent i = new  Intent(this, DrawerAddAd.class);
+            Intent i = new Intent(this, DrawerAddAd.class);
             startActivity(i);
         }
 
         if (id == R.id.nav_settings) {
-            Intent i = new  Intent(this, DrawerSettings.class);
+            Intent i = new Intent(this, DrawerSettings.class);
             startActivity(i);
         }
 
 
-       DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawerLayout.closeDrawer(GravityCompat.START);
         return true;
 
